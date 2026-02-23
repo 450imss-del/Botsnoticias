56df04031f380d8e52c81bd75b524fe9e9110bb8
450imss-del <450imss@gmail.com>
2026-02-22 19:12:02 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,6 @@
 self.addEventListener("install",e=>{
-  e.waitUntil(
-    caches.open("mencho-news-v2").then(c=>c.addAll(["./","index.html","manifest.json","icon.png"]))
-  );
+  e.waitUntil(caches.open("mencho-news-v3").then(c=>c.addAll(["./","index.html","manifest.json","icon.png"])));
 });
-
 self.addEventListener("fetch",e=>{
   e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
 });
